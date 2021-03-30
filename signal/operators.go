461e2e2cfbdef0cb5c685e188f21654b2266da42
Francisco Miamoto <fsmiamoto@gmail.com>
2021-03-30 20:11:54 -0300

    refactor: use anonymous functions for binary ops

--- signal/operators.go
+++ signal/operators.go
@@ -3,10 +3,18 @@ package signal
 type binaryOp func(a, b float64) float64
 type unaryOp func(a float64) float64
 
-func add(a, b float64) float64 {
-	return a + b
+func (s Signal) applyUnaryOperator(op unaryOp) Signal {
+	result := make(Signal, len(s))
+	for i := range result {
+		result[i] = op(s[i])
+	}
+	return result
 }
 
-func multiply(a, b float64) float64 {
-	return a * b
+func (s Signal) applyBinaryOperator(another Signal, op binaryOp) Signal {
+	result := make(Signal, len(s))
+	for i := range result {
+		result[i] = op(s[i], another[i])
+	}
+	return result
 }
--- signal/signal.go
+++ signal/signal.go
@@ -4,12 +4,16 @@ type Signal []float64
 
 // Multiply multiplies two signals, sample by sample, returning the resulting signal
 func (s Signal) Multiply(another Signal) Signal {
-	return s.applyBinaryOperator(multiply, another)
+	return s.applyBinaryOperator(another, func(a, b float64) float64 {
+		return a * b
+	})
 }
 
 // Add adds two signals, sample by sample, returning the resulting signal
 func (s Signal) Add(another Signal) Signal {
-	return s.applyBinaryOperator(add, another)
+	return s.applyBinaryOperator(another, func(a, b float64) float64 {
+		return a + b
+	})
 }
 
 // Scale scales a signal by a scalar factor
@@ -17,20 +21,4 @@ func (s Signal) Scale(factor float64) Signal {
 	return s.applyUnaryOperator(func(a float64) float64 {
 		return a * factor
 	})
-}
-
-func (s Signal) applyUnaryOperator(op unaryOp) Signal {
-	result := make(Signal, len(s))
-	for i := range result {
-		result[i] = op(s[i])
-	}
-	return result
-}
-
-func (s Signal) applyBinaryOperator(op binaryOp, another Signal) Signal {
-	result := make(Signal, len(s))
-	for i := range result {
-		result[i] = op(s[i], another[i])
-	}
-	return result
 }
